59fe586fca55a93449ee2afe560bc486e3a9ebca
AlCpRafa <ralonsocepeda@gmail.com>
2022-11-15 21:19:18 +0100

    finalizacion del formulario de login provisional

--- pages/login.php
+++ pages/login.php
@@ -1,44 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Login</title>
 </head>
+
 <body>
     <header class="header">
-        <h3 class="header__logo">OscarFlix</h3>
+        <h3 class="header__logo">Oscardede</h3>
     </header>
     <main class="main">
         <section class="login">
             <article class="login__art">
-                <h2>Iniciar sesion: </h2>
+                <h2 class="login__title">Iniciar sesion: </h2>
                 <form action="" method="post">
-                    <label for="user">Nombre de usuario</label>
-                    <input type="text" name="user" id="user">
-                    <label for="password">Contrasena</label>
-                    <input type="password" name="password" id="password">
-                    <input type="reset" value="Reset">
-                    <input type="submit" value="Enviar">
+                    <div class="login__cont">
+                        <label for="user">Nombre de usuario</label>
+                        <input type="text" name="user" id="user">
+                    </div>
+                    <div class="login__cont">
+                        <label for="password">Contrasena</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div class="login__cont">
+                        <input type="reset" value="Reset">
+                        <input type="submit" value="Enviar">
+                    </div>
                 </form>
             </article>
             <article class="login__art">
                 <h2>Registrate: </h2>
                 <form action="" method="post">
-                    <label for="name">Nombre: </label>
-                    <input type="text" name="name" id="name">
-                    <label for="surname">Apellidos: </label>
-                    <input type="text" name="surnmame" id="surname">
-                    <label for="email">Correo electronico: </label>
-                    <input type="email" name="email" id="email">
-                    <label for="setpass">Establece la contrasena: </label>
-                    <input type="password" name="setpass" id="setpass">
-                    <input type="reset" value="Reset">
-                    <input type="submit" value="Enviar">
+                    <div class="login__cont">
+                        <label for="name">Nombre de usuario: </label>
+                        <input type="text" name="name" id="name">
+                    </div>
+                    <div class="login__cont">
+                        <label for="setpass">Establece la contrasena: </label>
+                        <input type="password" name="setpass" id="setpass">
+                    </div>
+                    <div class="login__cont">
+                        <input type="reset" value="Reset">
+                        <input type="submit" value="Enviar">
+                    </div>
                 </form>
             </article>
         </section>
     </main>
 </body>
+
 </html>
